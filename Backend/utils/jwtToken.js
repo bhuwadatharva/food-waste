0665bf7580c04eb10806cd8ea8cb05ac202c9ce3
Atharva Nandkumar Bhuwad <bhuwad.atharva@gmail.com>
2025-02-18 19:28:46 +0530

    cs

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -7,7 +7,8 @@ export const generateToken = (user, message, statusCode, res) => {
   res.status(statusCode).cookie(cookieName, token, {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true, // Ensure the cookie is only accessible via HTTP(S)
-    secure:true
+    secure:true,
+    sameSite: "None",
   })
   .json({
     success: true,
